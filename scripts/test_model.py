d398e5c83a5d9951fc5c279c35704f5a413cbb3c
Erik Calcina <erik.calcina@gmail.com>
2024-04-17 15:21:17 +0200

    code adapted to meet requested formatting changes

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -12,7 +12,7 @@ from guidance import models, gen, select
 
 #TODO: Resolve the scripts problem
 sys.path.append("..")
-from src.models.guidance_generate import Generate
+from src.models.generate import Generate
 
 def main(hparams):
     base_model_name = hparams.model
@@ -22,24 +22,19 @@ def main(hparams):
 
     f = open(save_results+".txt", "w")
 
+    #import model
+    device = "cuda"
+    model = AutoModelForCausalLM.from_pretrained(base_model_name, load_in_4bit=True)
+    model = PeftModel.from_pretrained(model, adapter).to(device)
+
     tokenizer = AutoTokenizer.from_pretrained(base_model_name)
     tokenizer.padding_side = 'right'
-    #tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token = tokenizer.eos_token
 
-    model = AutoModelForCausalLM.from_pretrained(
-        base_model_name,
-        load_in_4bit=True,
-        device_map="auto",
-    )
-
-    model = PeftModel.from_pretrained(model, adapter)
-
-    model = models.Transformers(model, tokenizer, echo=False)
-
-    #Import test dataset
+    #import test dataset
     test_dataset = load_from_disk(dataset)
 
-    F1score = defaultdict(list)
+    F1_score = defaultdict(list)
 
     compare = ""
     #entities = ["Age"]
@@ -47,22 +42,22 @@ def main(hparams):
     entities = ["Age", "Sex", "Sign_symptom", "Lab_value", "Biological_structure", "Diagnostic_procedure"]
 
     for instance in tqdm(range(len(test_dataset['input']))):
-        #TODO: Remove "strange" character (guidance returning error)
         text = test_dataset['input'][instance].replace(" ", " ")
         text = text.replace("℃", "°C")
 
-        extract = Generate.extract(model, entities, text, max_length=400) #250 first try
+        extract = Generate.extract(model, tokenizer, entities, text, max_length=2000)
 
         compare = compare + test_dataset[instance]["input"] + "\n\n"
 
         for x in json.loads(test_dataset['output'][instance]):
+
             if x not in entities:
                 continue
 
             if x not in extract:
                 text = f"{x} \nReal: {json.loads(test_dataset['output'][instance])[x]} \nGene: Failed \nMatch: \n\n"
                 compare = compare + text
-                F1score[x].append(0)
+                F1_score[x].append(0)
                 continue
 
             if json.loads(test_dataset['output'][instance])[x] is None:
@@ -72,6 +67,7 @@ def main(hparams):
 
             real = list(set(json.loads(test_dataset['output'][instance])[x]))
             gene = list(set(extract[x]))
+
             match = list(set([r for r in real if r in gene]))
 
             lenmatch = len(match)
@@ -89,17 +85,17 @@ def main(hparams):
             else:
                 f1 = 2*(precision*recall) / (precision+recall)
 
-            F1score[x].append(f1) 
-                    
+            F1_score[x].append(f1) 
+                
         compare = compare + "\n\n\n"
     
-    compare = compare + json.dumps(averageF1Dict(F1score))
+    compare = compare + json.dumps(average_F1_dict(F1_score))
 
     f.write(compare)
     f.close()
 
 
-def averageF1Dict(di):
+def average_F1_dict(di):
     return {k:sum(v)/len(v) for k,v in di.items()}
 
 
--- scripts/training_shuffled.py
+++ scripts/training_shuffled.py
@@ -1,43 +1,50 @@
+import os
 from argparse import ArgumentParser
+from datasets import load_from_disk
+
 from peft import LoraConfig, TaskType
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
-from datasets import load_from_disk
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer
-from trl import AutoModelForCausalLMWithValueHead
-import os
 
-# EXAMPLE: python training_shuffled.py --model "AdaptLLM/medicine-chat" --dataset "../data/datasetTrainShuffled" --output "../models/medicine-chat-Erik-wandb" --project "PREPARE"
+from dotenv import load_dotenv, dotenv_values 
+
 def main(hparams):
     model_id = hparams.model
     dataset_path = hparams.dataset
+    hg_dataset_test = hparams.testdataset
     save_model_path = hparams.output
-    os.environ["WANDB_PROJECT"] = hparams.project
 
-    access_token = "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"
+    os.environ['WANDB_DISABLED'] = 'true'
+    if hparams.project is not None:
+        os.environ["WANDB_PROJECT"] = hparams.project
+
+    # loading variables from .env file
+    load_dotenv()
+    access_token = os.getenv("access_token")
 
     # TODO: check the lora config parameters, what do they mean and what they impact
     peft_config = LoraConfig(task_type=TaskType.CAUSAL_LM, inference_mode=False, r=64, lora_alpha=32, lora_dropout=0.1)
 
-
-    #model_id = 'AdaptLLM/medicine-chat'# go for a smaller model if you dont have the VRAM
-    model = AutoModelForCausalLM.from_pretrained(model_id, token=access_token, device_map="auto", load_in_4bit=True) # , peft_config=peft_config
+    model = AutoModelForCausalLM.from_pretrained(model_id, token=access_token, device_map="auto", load_in_4bit=True)
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     tokenizer.padding_side = 'right'
     tokenizer.pad_token = tokenizer.eos_token
 
-
-    #hg_dataset = load_from_disk("../data/dataset")
     hg_dataset = load_from_disk(dataset_path)
-    #hg_dataset = hg_dataset.train_test_split(train_size=0.9)
-
-    hg_datasetTEST = load_from_disk("../data/datasetTestShuffledEntire")
-
 
     def formatting_prompts_func(example):
         output_texts = []
         for i in range(len(example['input'])):
-            text = f"### Your goal is to extract structured information from the user's input that matches json format. When extracting information please make sure it matches the type information exactly. Extract the following entities: {example['keys'][i]} Do not add any extra entities.\n\n### Input:\n{example['input'][i]}\n\n ### Output:\n{example['output'][i]}"
-            #print(text)
+            if isinstance(example['keys'][i], list):
+                text = f"### Your goal is to extract structured information from the user's input that matches json format. When extracting information please make sure it matches the type information exactly. Extract the following entities: {example['keys'][i]} Do not add any extra entities.\n\n### Input:\n{example['input'][i]}\n\n ### Output: \n{example['output'][i]}"
+                output_texts.append(text)
+                continue
+            keys = example['keys'][i]
+            text = f"### Your goal is to extract structured information from the user's input that matches json format. When extracting information please make sure it matches the type information exactly. Extract the following entities: {keys} Do not add any extra entities.\n\n### Input:\n{example['input'][i]}\n\n ### Output: \n{example['output'][i]}"
+            #TODO: Remove "strange" character (guidance returning error)
+            text = text.replace(" ", " ")
+            text = text.replace("℃", "°C")
+             
             output_texts.append(text)
         return output_texts
 
@@ -46,18 +53,17 @@ def main(hparams):
 
     # TODO: play around with the traning arguments, specifically the optimizer ones (learning rate, batch size, epochs)
     args = TrainingArguments(
-        report_to="wandb", # enable logging to W&B
-        output_dir="../models/AdaptLLM/medicine-chat-Erikk",
+        report_to="wandb",
+        output_dir=save_model_path,
         learning_rate=2e-4,
         num_train_epochs=1,
         logging_steps=10,
         gradient_checkpointing=True,
-        per_device_train_batch_size = 2,
+        per_device_train_batch_size = 1,
         per_device_eval_batch_size = 2,
         do_eval=True,
         eval_steps=10,
         evaluation_strategy = "steps",
-        #save_strategy="epoch",
     )
 
     # TODO: check the SFTTrainer parameters and how to train the model
@@ -66,28 +72,26 @@ def main(hparams):
     trainer = SFTTrainer(
         model,
         train_dataset=hg_dataset,
-        #eval_dataset=hg_dataset["test"],
-        eval_dataset=hg_datasetTEST,
+        eval_dataset=hg_dataset_test,
         formatting_func=formatting_prompts_func,
         data_collator=collator,
-        max_seq_length=2048,
+        max_seq_length=4096,
         args=args,
         peft_config=peft_config,
     )
 
     trainer.train()
 
-    # ! replaced approach for saving the models
-    #model.save_pretrained("../models/AdaptLLM/medicine-chat-Erik")
-    #trainer.save_model("../models/AdaptLLM/medicine-chat-Erik")
     trainer.save_model(save_model_path)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--model", type=str, default=None) #--> model_id (hugging face)
-    parser.add_argument("--dataset", type=str, default=None) #--> path to training dataset
-    parser.add_argument("--output", type=str, default=None) #--> path for saving the model
-    parser.add_argument("--project", type=str, default=None) #--> path for saving the model
+    parser.add_argument("--model", type=str, default=None, help="model_id (hugging face)")
+    parser.add_argument("--dataset", type=str, default=None, help="path to training dataset")
+    parser.add_argument("--testdataset", type=str, default=None, help="path to test dataset")
+    parser.add_argument("--output", type=str, default=None, help="path for saving the model")
+    parser.add_argument("--project", type=str, default=None, help="wndb project")
     args = parser.parse_args()
 
     main(args)
+    
